23e8c42e26c409c85c0616c9af400631c628e43b
webfourmi <134513115+webfourmi@users.noreply.github.com>
2026-06-08 18:14:39 +0200

    V5.0.1 update config pages , ajout goal et planning

--- app-config.js
+++ app-config.js
@@ -15,13 +15,13 @@
 
 window.FitnessRpgConfig = {
   // Version technique utilisée dans les petits labels.
-  version: "0.5.0.0",
+  version: "0.5.0.1",
 
   // Version lisible affichée dans le header et l’onglet.
-  displayVersion: "V5.0.0",
+  displayVersion: "V5.0.1",
 
   // Version utilisée pour le cache des fichiers CSS/JS dans index.html.
-  assetVersion: "5.0.0",
+  assetVersion: "5.0.1",
 
   // ------------------------------------------------------------
   // Clés de sauvegarde locale
@@ -210,7 +210,10 @@ window.FitnessRpgConfig = {
     "music",
     "badges",
     "journal",
-    "weight"
+    "weight",
+    "goal",
+    "planning"
+
   ]
 };
 
