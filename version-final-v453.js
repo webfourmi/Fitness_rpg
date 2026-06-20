061bcd2768898db86f2aa5271b80a36def4aee3f
webfourmi <134513115+webfourmi@users.noreply.github.com>
2026-06-20 10:16:31 +0200

    Delete version-final-v453.js

--- version-final-v453.js
+++
@@ -1,9 +0,0 @@
-function initVersionFinalV453() {
-  window.FitnessRpgConfig?.setVersionLabels?.();
-}
-
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", initVersionFinalV453);
-} else {
-  initVersionFinalV453();
-}
