0b2cf82749a711ec42a431e5c2d5f609356bc1c1
webfourmi <134513115+webfourmi@users.noreply.github.com>
2026-08-15 19:05:22 +0200

    Update version numbers to 0.6.6.3

--- app-config.js
+++ app-config.js
@@ -15,13 +15,13 @@
 
 window.FitnessRpgConfig = {
   // Version technique utilisée dans les petits labels.
-  version: "0.6.6.2",
+  version: "0.6.6.3",
 
   // Version lisible affichée dans le header et l’onglet.
-  displayVersion: "V6.6.2",
+  displayVersion: "V6.6.3",
 
   // Version utilisée pour le cache des fichiers CSS/JS dans index.html.
-  assetVersion: "6.6.2",
+  assetVersion: "6.6.3",
 
   // ------------------------------------------------------------
   // Clés de sauvegarde locale
